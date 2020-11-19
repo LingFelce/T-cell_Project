6e710b6ae6812bb2e2b9f18f9782a337d1dfe8c6
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-11-19 13:48:08 +0000

    circle plots for cd4 m24, cd8, separated a + b

--- tcR_circle.R
+++ tcR_circle.R
@@ -156,9 +156,7 @@ setwd('/t1-data/user/lfelce/TCR_analysis')
 
 cd4_s34 <- as.data.frame(rbind(cd4_df[c(1:134,282:400),]))
 
-cd4_m24_mild <- as.data.frame(rbind(cd4_df[c(135:281,401:555),]))
-
-cd4_m24_severe <- as.data.frame(rbind(cd4_df[c(1556:852),]))
+cd4_m24 <- as.data.frame(rbind(cd4_df[c(135:281,401:852),]))
 
 
 # need to plot table with variable genes as rows and junction genes as columns
@@ -201,6 +199,91 @@ circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
   circos.axis(h = "top", labels.cex = 0.25, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
 }, bg.border = NA)
 
-circos.info()
 dev.off()
+
+
+# need to plot table with variable genes as rows and junction genes as columns
+
+cd4_m24_table <- as.data.frame.matrix(table(cd4_m24$V.gene, cd4_m24$J.gene))
+cd4_m24_table <- as.matrix(cd4_m24_table[-1,-1])
+
+# alpha chain
+cd4_m24_table_a <- as.data.frame.matrix(rbind(cd4_m24_table[c(1:46),]))
+cd4_m24_table_a <- as.data.frame.matrix(cbind(cd4_m24_table_a[,c(1:49)]))
+cd4_m24_table_a <- as.matrix(cd4_m24_table_a)
+
+pdf('cd4_m24_a_chorddiagram.pdf', width = 12, height = 8, useDingbats = FALSE)
+circos.clear()
+set.seed(999)
+chordDiagram(cd4_m24_table_a, annotationTrack = "grid", preAllocateTracks = 1)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.25, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+dev.off()
+
+# beta chain
+cd4_m24_table_b <- as.data.frame.matrix(rbind(cd4_m24_table[c(47:110),]))
+cd4_m24_table_b <- as.data.frame.matrix(cbind(cd4_m24_table_b[,c(50:63)]))
+cd4_m24_table_b <- as.matrix(cd4_m24_table_b)
+
+pdf('cd4_m24_b_chorddiagram.pdf', width = 12, height = 8, useDingbats = FALSE)
+circos.clear()
+set.seed(999)
+chordDiagram(cd4_m24_table_b, annotationTrack = "grid", preAllocateTracks = 1)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.25, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+
+dev.off()
+
+# cd8 tabulate and separate alpha and beta
+
+cd8_df <- as.data.frame(cd8_df)
+cd8_table <- as.data.frame.matrix(table(cd8_df$V.gene, cd8_df$J.gene))
+
+# alpha chain
+cd8_table_a <- as.data.frame.matrix(rbind(cd8_table[c(1:41),]))
+cd8_table_a <- as.data.frame.matrix(cbind(cd8_table_a[,c(1:37)]))
+cd8_table_a <- as.matrix(cd8_table_a)
+
+pdf('cd8_a_chorddiagram.pdf', width = 12, height = 8, useDingbats = FALSE)
+circos.clear()
+set.seed(999)
+chordDiagram(cd8_table_a, annotationTrack = "grid", preAllocateTracks = 1)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.25, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+dev.off()
+
+# beta chain
+cd8_table_b <- as.data.frame.matrix(rbind(cd8_table[c(42:91),]))
+cd8_table_b <- as.data.frame.matrix(cbind(cd8_table_b[,c(38:51)]))
+cd8_table_b <- as.matrix(cd8_table_b)
+
+pdf('cd8_b_chorddiagram.pdf', width = 12, height = 8, useDingbats = FALSE)
+circos.clear()
+set.seed(999)
+chordDiagram(cd8_table_b, annotationTrack = "grid", preAllocateTracks = 1)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.25, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+
+dev.off()
+
 
