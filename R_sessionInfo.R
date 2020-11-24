a76367d687d6710184b68327f848e73011ea9e0d
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-11-24 14:20:39 +0000

    session info

---
+++ R_sessionInfo.R
@@ -0,0 +1,51 @@
+> sessionInfo()
+R version 4.0.1 (2020-06-06)
+Platform: x86_64-pc-linux-gnu (64-bit)
+Running under: CentOS Linux 7 (Core)
+
+Matrix products: default
+BLAS:   /Filers/package/R-base/4.0.1/lib64/R/lib/libRblas.so
+LAPACK: /Filers/package/R-base/4.0.1/lib64/R/lib/libRlapack.so
+
+locale:
+ [1] LC_CTYPE=en_GB.UTF-8       LC_NUMERIC=C               LC_TIME=en_GB.UTF-8       
+ [4] LC_COLLATE=en_GB.UTF-8     LC_MONETARY=en_GB.UTF-8    LC_MESSAGES=en_GB.UTF-8   
+ [7] LC_PAPER=en_GB.UTF-8       LC_NAME=C                  LC_ADDRESS=C              
+[10] LC_TELEPHONE=C             LC_MEASUREMENT=en_GB.UTF-8 LC_IDENTIFICATION=C       
+
+attached base packages:
+[1] stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+ [1] Seurat_3.2.1        TCRtools_0.0.0.9003 circlize_0.4.10     data.table_1.13.0   tcR_2.3.2          
+ [6] igraph_1.2.5        reshape2_1.4.4      gridExtra_2.3       dplyr_1.0.2         ggplot2_3.3.2      
+
+loaded via a namespace (and not attached):
+  [1] Rtsne_0.15            colorspace_1.4-1      deldir_0.1-25         ellipsis_0.3.1       
+  [5] ggridges_0.5.2        rprojroot_1.3-2       GlobalOptions_0.1.2   rstudioapi_0.11      
+  [9] spatstat.data_1.4-3   leiden_0.3.3          listenv_0.8.0         farver_2.0.3         
+ [13] remotes_2.1.1         ggrepel_0.8.2         fansi_0.4.1           codetools_0.2-16     
+ [17] splines_4.0.1         polyclip_1.10-0       jsonlite_1.7.0        ica_1.0-2            
+ [21] cluster_2.1.0         png_0.1-7             uwot_0.1.8            shiny_1.5.0          
+ [25] sctransform_0.2.1     compiler_4.0.1        httr_1.4.2            backports_1.1.9      
+ [29] assertthat_0.2.1      Matrix_1.2-18         fastmap_1.0.1         lazyeval_0.2.2       
+ [33] cli_2.0.2             later_1.1.0.1         htmltools_0.5.0       prettyunits_1.1.1    
+ [37] tools_4.0.1           rsvd_1.0.3            gtable_0.3.0          glue_1.4.2           
+ [41] RANN_2.6.1            rappdirs_0.3.1        Rcpp_1.0.5            spatstat_1.64-1      
+ [45] vctrs_0.3.4           nlme_3.1-148          lmtest_0.9-37         stringr_1.4.0        
+ [49] globals_0.13.1        ps_1.3.4              mime_0.9              miniUI_0.1.1.1       
+ [53] lifecycle_0.2.0       irlba_2.3.3           goftest_1.2-2         stringdist_0.9.6     
+ [57] future_1.20.1         MASS_7.3-51.6         zoo_1.8-8             scales_1.1.1         
+ [61] promises_1.1.1        spatstat.utils_1.17-0 parallel_4.0.1        RColorBrewer_1.1-2   
+ [65] curl_4.3              reticulate_1.16       pbapply_1.4-2         rpart_4.1-15         
+ [69] stringi_1.4.6         checkmate_2.0.0       pkgbuild_1.1.0        shape_1.4.4          
+ [73] rlang_0.4.7           pkgconfig_2.0.3       matrixStats_0.56.0    lattice_0.20-41      
+ [77] tensor_1.5            ROCR_1.0-11           purrr_0.3.4           patchwork_1.0.0      
+ [81] htmlwidgets_1.5.1     labeling_0.3          cowplot_1.1.0         processx_3.4.3       
+ [85] tidyselect_1.1.0      parallelly_1.21.0     RcppAnnoy_0.0.16      plyr_1.8.6           
+ [89] magrittr_1.5          R6_2.4.1              generics_0.0.2        mgcv_1.8-31          
+ [93] pillar_1.4.6          withr_2.2.0           fitdistrplus_1.1-1    abind_1.4-5          
+ [97] survival_3.1-12       tibble_3.0.3          future.apply_1.5.0    crayon_1.3.4         
+[101] KernSmooth_2.23-17    utf8_1.1.4            plotly_4.9.2.1        grid_4.0.1           
+[105] callr_3.4.3           digest_0.6.25         xtable_1.8-4          tidyr_1.1.2          
+[109] httpuv_1.5.4          munsell_0.5.0         viridisLite_0.3.0
