737ee55cf1424c75c54179251d982b739d31f487
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-12-04 19:03:48 +0000

    updated for sctransform

--- R_sessionInfo.R
+++ R_sessionInfo.R
@@ -19,52 +19,48 @@ attached base packages:
 [1] stats     graphics  grDevices utils     datasets  methods   base     
 
 other attached packages:
- [1] forcats_0.5.0     stringr_1.4.0     purrr_0.3.4       readr_1.3.1      
- [5] tidyr_1.1.2       tibble_3.0.3      tidyverse_1.3.0   ggplot2_3.3.2    
- [9] cowplot_1.1.0     data.table_1.13.0 patchwork_1.0.0   dplyr_1.0.2      
-[13] Seurat_3.9.9.9010
+ [1] forcats_0.5.0          stringr_1.4.0          purrr_0.3.4           
+ [4] readr_1.3.1            tidyr_1.1.2            tibble_3.0.3          
+ [7] tidyverse_1.3.0        data.table_1.13.0      ggplot2_3.3.2         
+[10] cowplot_1.1.0          patchwork_1.0.0        dplyr_1.0.2           
+[13] SeuratDisk_0.0.0.9013  sctransform_0.3.1.9003 Seurat_3.9.9.9010     
 
 loaded via a namespace (and not attached):
   [1] Rtsne_0.15            colorspace_1.4-1      deldir_0.1-25        
   [4] ellipsis_0.3.1        ggridges_0.5.2        fs_1.5.0             
-  [7] rstudioapi_0.11       spatstat.data_1.4-3   rstan_2.21.2         
- [10] farver_2.0.3          leiden_0.3.3          listenv_0.8.0        
- [13] ggrepel_0.8.2         fansi_0.4.1           lubridate_1.7.9      
- [16] xml2_1.3.2            codetools_0.2-16      splines_4.0.1        
- [19] knitr_1.29            polyclip_1.10-0       jsonlite_1.7.0       
- [22] packrat_0.5.0         broom_0.7.0           ica_1.0-2            
- [25] cluster_2.1.0         dbplyr_1.4.4          png_0.1-7            
- [28] uwot_0.1.9.9000       shiny_1.5.0           sctransform_0.3.1    
- [31] compiler_4.0.1        httr_1.4.2            backports_1.1.9      
- [34] assertthat_0.2.1      Matrix_1.2-18         fastmap_1.0.1        
- [37] lazyeval_0.2.2        cli_2.0.2             later_1.1.0.1        
- [40] prettyunits_1.1.1     htmltools_0.5.0       tools_4.0.1          
- [43] rsvd_1.0.3            igraph_1.2.5          gtable_0.3.0         
- [46] glue_1.4.2            RANN_2.6.1            reshape2_1.4.4       
- [49] rappdirs_0.3.1        V8_3.2.0              Rcpp_1.0.5           
- [52] spatstat_1.64-1       cellranger_1.1.0      vctrs_0.3.4          
- [55] nlme_3.1-148          lmtest_0.9-37         xfun_0.16            
- [58] ps_1.3.4              globals_0.13.1        rvest_0.3.6          
- [61] mime_0.9              miniUI_0.1.1.1        lifecycle_0.2.0      
- [64] irlba_2.3.3           goftest_1.2-2         future_1.20.1        
- [67] MASS_7.3-51.6         zoo_1.8-8             scales_1.1.1         
- [70] hms_0.5.3             promises_1.1.1        spatstat.utils_1.17-0
- [73] parallel_4.0.1        inline_0.3.15         RColorBrewer_1.1-2   
- [76] curl_4.3              reticulate_1.16       pbapply_1.4-2        
- [79] gridExtra_2.3         loo_2.3.1             StanHeaders_2.21.0-5 
- [82] rpart_4.1-15          stringi_1.4.6         pkgbuild_1.1.0       
- [85] rlang_0.4.7           pkgconfig_2.0.3       matrixStats_0.56.0   
- [88] lattice_0.20-41       ROCR_1.0-11           tensor_1.5           
- [91] labeling_0.3          htmlwidgets_1.5.1     processx_3.4.3       
- [94] tidyselect_1.1.0      parallelly_1.21.0     RcppAnnoy_0.0.16     
- [97] plyr_1.8.6            magrittr_1.5          R6_2.4.1             
-[100] generics_0.0.2        DBI_1.1.0             pillar_1.4.6         
-[103] haven_2.3.1           withr_2.2.0           mgcv_1.8-31          
-[106] fitdistrplus_1.1-1    survival_3.1-12       abind_1.4-5          
-[109] future.apply_1.5.0    modelr_0.1.8          crayon_1.3.4         
-[112] KernSmooth_2.23-17    plotly_4.9.2.1        grid_4.0.1           
-[115] readxl_1.3.1          callr_3.4.3           blob_1.2.1           
-[118] reprex_0.3.0          digest_0.6.25         xtable_1.8-4         
-[121] httpuv_1.5.4          RcppParallel_5.0.1    stats4_4.0.1         
-[124] munsell_0.5.0         viridisLite_0.3.0    
-> 
+  [7] rstudioapi_0.11       spatstat.data_1.4-3   leiden_0.3.3         
+ [10] listenv_0.8.0         ggrepel_0.8.2         bit64_0.9-7          
+ [13] lubridate_1.7.9       fansi_0.4.1           xml2_1.3.2           
+ [16] codetools_0.2-16      splines_4.0.1         knitr_1.29           
+ [19] polyclip_1.10-0       jsonlite_1.7.0        packrat_0.5.0        
+ [22] broom_0.7.0           ica_1.0-2             cluster_2.1.0        
+ [25] dbplyr_1.4.4          png_0.1-7             uwot_0.1.9.9000      
+ [28] shiny_1.5.0           compiler_4.0.1        httr_1.4.2           
+ [31] backports_1.1.9       assertthat_0.2.1      Matrix_1.2-18        
+ [34] fastmap_1.0.1         lazyeval_0.2.2        cli_2.0.2            
+ [37] later_1.1.0.1         htmltools_0.5.0       tools_4.0.1          
+ [40] rsvd_1.0.3            igraph_1.2.5          gtable_0.3.0         
+ [43] glue_1.4.2            RANN_2.6.1            reshape2_1.4.4       
+ [46] rappdirs_0.3.1        Rcpp_1.0.5            spatstat_1.64-1      
+ [49] cellranger_1.1.0      vctrs_0.3.4           nlme_3.1-148         
+ [52] lmtest_0.9-37         xfun_0.16             globals_0.13.1       
+ [55] rvest_0.3.6           mime_0.9              miniUI_0.1.1.1       
+ [58] lifecycle_0.2.0       irlba_2.3.3           goftest_1.2-2        
+ [61] future_1.20.1         MASS_7.3-51.6         zoo_1.8-8            
+ [64] scales_1.1.1          hms_0.5.3             promises_1.1.1       
+ [67] spatstat.utils_1.17-0 parallel_4.0.1        RColorBrewer_1.1-2   
+ [70] reticulate_1.16       pbapply_1.4-2         gridExtra_2.3        
+ [73] rpart_4.1-15          stringi_1.4.6         rlang_0.4.7          
+ [76] pkgconfig_2.0.3       matrixStats_0.56.0    lattice_0.20-41      
+ [79] ROCR_1.0-11           tensor_1.5            htmlwidgets_1.5.1    
+ [82] bit_1.1-15.2          tidyselect_1.1.0      parallelly_1.21.0    
+ [85] RcppAnnoy_0.0.16      plyr_1.8.6            magrittr_1.5         
+ [88] R6_2.4.1              generics_0.0.2        DBI_1.1.0            
+ [91] haven_2.3.1           pillar_1.4.6          withr_2.2.0          
+ [94] mgcv_1.8-31           fitdistrplus_1.1-1    survival_3.1-12      
+ [97] abind_1.4-5           future.apply_1.5.0    modelr_0.1.8         
+[100] crayon_1.3.4          hdf5r_1.3.3           KernSmooth_2.23-17   
+[103] plotly_4.9.2.1        readxl_1.3.1          grid_4.0.1           
+[106] blob_1.2.1            reprex_0.3.0          digest_0.6.25        
+[109] xtable_1.8-4          httpuv_1.5.4          munsell_0.5.0        
+[112] viridisLite_0.3.0 
