8392ea924c278236c169cee6316b07b48e8b5b5e
LingFelce <55535650+LingFelce@users.noreply.github.com>
2020-12-22 19:20:07 +0000

    editing Cell Ranger VDJ command

--- cellranger_vdj.sh
+++ cellranger_vdj.sh
@@ -27,6 +27,30 @@ fastqs="outs/fastq_path/"
 transcriptome="refdata-cellranger-vdj-GrCh38-alts-ensembl-2.0.0"
 ref="/databank/10x-rangers/${transcriptome}"
 
+################### My Script - generate individual .sh for each .fastq.gz file then send to queue separately ####################
 
+cd /t1-data/user/lfelce/MiXCR/1131-TP-1_CD8_new_input/
+DIR=/t1-data/user/lfelce/MiXCR/1131-TP-1_CD8_new_output/
+
+for NAME in $(find . -name '*_R1_001.fastq.gz' -printf "%f\n" | sed 's/_R1_001.fastq.gz//'); do
+ 
+echo "$NAME"
+
+p1='_R1_001.fastq.gz'
+p2='_R2_001.fastq.gz' 
+
+echo -e '#!/bin/sh
+#SBATCH --partition=batch
+#SBATCH --nodes=1
+#SBATCH --mem=128G
+
+module load mixcr
+
+cd /t1-data/user/lfelce/MiXCR/1131-TP-1_CD8_new_output/
 
 cellranger vdj --id=
+
+done
+
+#################################
+mixcr analyze amplicon -s hsa --starting-material rna --5-end no-v-primers --3-end c-primers --adapters no-adapters --contig-assembly --only-productive /t1-data/user/lfelce/MiXCR/1131-TP-1_CD8_new_input/'$NAME$p1 '/t1-data/user/lfelce/MiXCR/1131-TP-1_CD8_new_input/'$NAME$p2 $NAME '--receptor-type tcr' > $DIR'script/'$NAME'.sh'
