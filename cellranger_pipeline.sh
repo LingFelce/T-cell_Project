147db7abe364c3e6900a821eed77fe29195ef47b
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-02-01 11:43:57 +0000

    cell ranger count

--- cellranger_pipeline.sh
+++ cellranger_pipeline.sh
@@ -31,3 +31,24 @@ cellranger mkfastq --id=Dong_171220_10x \
 --run=/t1-data/user/lfelce/10x_DONG171220/ \
 --csv=/t1-data/user/lfelce/10x_DONG171220/Dong_171220_sample_sheet.csv
 
+# cellranger_count.sh
+
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --job-name=count
+#SBATCH --nodes=1
+#SBATCH --mem=128G
+#SBATCH --time=07-00:00:00
+#SBATCH --output=%j_%x.out
+#SBATCH --error=%j_%x.err
+
+
+cd /t1-data/user/lfelce/10x_DONG171220/Dong_171220_10x/outs/fastq_path/
+
+module load cellranger/5.0.0
+
+cellranger count --id=count_Dong_171220_10x \
+--fastqs=/t1-data/user/lfelce/10x_DONG171220/Dong_171220_10x/outs/fastq_path/ \
+--sample=Dong171220 \
+--transcriptome=/databank/10x-rangers/refdata-cellranger-GRCh38-3.0.0
+
