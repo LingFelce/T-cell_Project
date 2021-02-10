451de64f93aa01b69029b05d9729993d202ceedb
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-02-10 12:12:04 +0000

    use agrep for distance

--- tcR_similar_cdr3_sc_group.R
+++ tcR_similar_cdr3_sc_group.R
@@ -8,76 +8,29 @@ library(dplyr)
 
 ## CD8 NP16 ##
 
-setwd('/t1-data/user/lfelce/TCR_analysis/cd8_np16_new/')
-
-# create list of file names
-tra_list <- list.files(path = ".", recursive = TRUE,
-                       pattern = "\\TRA.txt$", 
-                       full.names = TRUE)
-tra_list <- tra_list %>% str_replace("./*", "")
-
-trb_list <- list.files(path = ".", recursive = TRUE,
-                       pattern = "\\TRB.txt$", 
-                       full.names = TRUE)
-trb_list <- trb_list %>% str_replace("./*", "")
+# calculating similarity - CDR3 alpha
+np16 <- read.csv("/t1-data/user/lfelce/TCR_analysis/shared_cdr3_cd8_np16.csv", header=TRUE)
 
 
-# parse in files with tcR
-mixcr_a <- parse.file.list(tra_list, "mixcr")
-mixcr_b <- parse.file.list(trb_list, "mixcr")
-
-# sort alphabetically
-mixcr_a <- mixcr_a[order(names(mixcr_a))]
-mixcr_b <- mixcr_b[order(names(mixcr_b))]
-
-# create list of cell numbers and cell names
-# different lengths so same cell will be different number in a or b
-mixcr_a_names <- as.data.frame(names(mixcr_a))
-mixcr_b_names <- as.data.frame(names(mixcr_b))
-
-mixcr_a_names <-tibble::rownames_to_column(mixcr_a_names, "cell_number")
-mixcr_b_names <- tibble::rownames_to_column(mixcr_b_names, "cell_number")
-
-# rename columns
-colnames(mixcr_a_names) <- c("cell_number", "cell_name")
-colnames(mixcr_b_names) <- c("cell_number", "cell_name")
-
-# convert mixcr lists to dataframe with Read count and proportion and V and J gene info
-# removed bulk samples for now
-# keep cells with 1 or 2 alphas and 1 beta
-
-# mixcr_a
 datalist = list()
-for (i in (1:length(mixcr_a))) {
-  dat <- data.frame(c(mixcr_a[[i]][3], mixcr_a[[i]][4], mixcr_a[[i]][6],mixcr_a[[i]][7], mixcr_a[[i]][8]))
+for (i in (1:length(np16))) {
+  dat <- data.frame(agrep(pattern=np16$CDR3a[i], x=np16$CDR3a, 
+                          max.distance=0.3,value=TRUE, fixed=TRUE))
   dat$i <- i # keep track of which iteration produced it
   datalist[[i]] <- dat # add it to list
 }
-# combine columns for each cell and filter to keep cells with 1 or 2 alphas
 big_data = do.call(rbind, datalist)
-tra <- big_data %>% group_by(i) %>% filter(n() <= 2)
-colnames(tra) <- c("clone_count", "clone_fraction", "CDR3_alpha", "TRAV", "TRAJ", "cell_number")
-tra <- merge(tra, mixcr_a_names, by="cell_number")
+colnames(big_data) <- c("sequence", "group")
+alpha_shared <- merge(np16, big_data, by.x="CDR3a", by.y="sequence")
 
 
-# mixcr_b
 datalist = list()
-for (i in (1:length(mixcr_b))) {
-  dat <- data.frame(c(mixcr_b[[i]][3], mixcr_b[[i]][4], mixcr_b[[i]][6], mixcr_b[[i]][7], mixcr_b[[i]][8]))
+for (i in (1:length(np16))) {
+  dat <- data.frame(agrep(pattern=np16$CDR3b[i], x=np16$CDR3b, 
+                          max.distance=0.3,value=TRUE, fixed=TRUE))
   dat$i <- i # keep track of which iteration produced it
   datalist[[i]] <- dat # add it to list
 }
-# combine columns for each cell
 big_data = do.call(rbind, datalist)
-trb <- big_data %>% group_by(i) %>% filter(n() == 1)
-colnames(trb) <- c("clone_count", "clone_fraction","CDR3_beta", "TRBV", "TRBJ", "cell_number")
-trb <- merge(trb, mixcr_b_names, by="cell_number")
-
-
-# combine TRA and TRB dataframes
-
-np16 <- merge(tra,trb, by="cell_name")
-
-np16 <- mutate(np16, alpha=paste(TRAV, TRAJ, sep="_"))
-
-np16 <- mutate(np16, beta=paste(TRBV, TRBJ, sep="_"))
+colnames(big_data) <- c("sequence", "group")
+beta_shared <- merge(np16, big_data, by.x="CDR3b", by.y="sequence")
