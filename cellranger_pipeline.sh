b4bb480decbc2f022f7582bdd0695c5cddb7e081
LingFelce <55535650+LingFelce@users.noreply.github.com>
2021-02-12 15:48:53 +0000

    updated to do cellranger count in CCB or BMRC clusters

--- cellranger_pipeline.sh
+++ cellranger_pipeline.sh
@@ -62,7 +62,10 @@ cd /t1-data/user/lfelce/10x_DONG171220/Dong_171220_10x/outs/fastq_path/
 
 module load cellranger/5.0.0
 
-cellranger count --id=count_Dong_171220_10x --fastqs=/t1-data/user/lfelce/10x_DONG171220/Dong_171220_10x/outs/fastq_path/ --sample=Dong_171220_GEX --transcriptome=/databank/10x-rangers/refdata-cellranger-GRCh38-3.0.0
+cellranger count --id=count_Dong_171220_10x \
+--fastqs=/t1-data/user/lfelce/10x_DONG171220/Dong_171220_10x/outs/fastq_path/ \
+--sample=Dong_171220_GEX \
+--transcriptome=/databank/10x-rangers/refdata-cellranger-GRCh38-3.0.0
 
 # cellranger aggr required, only for combining multiple runs of cellranger count to normalise runs to same sequencing depth.
 # can use output from cellranger count filtered_feature_bc_matrix into Seurat
@@ -138,4 +141,16 @@ fastqs,sample,library_type
 /gpfs2/well/jknight/users/jln789/10x_Dong171220/,Dong_171220_Cell_Surface,Antibody Capture
 /gpfs2/well/jknight/users/jln789/10x_Dong171220/,Dong_171220_GEX,Gene Expression
 
+# cellranger count for just gene expression
+#!/bin/bash
+#$ -wd /well/jknight/users/jln789/10x_Dong050121/fastq_Dong050121/outs/fastq_path/
+#$ -q short.qc
 
+cd /well/jknight/users/jln789/10x_Dong050121/fastq_Dong050121/outs/fastq_path/
+
+module load CellRanger/5.0.0
+
+cellranger count --id=count_T1_Dong050121 \
+--fastqs=/well/jknight/users/jln789/10x_Dong050121/fastq_Dong050121/outs/fastq_path/ \
+--sample=Dong_050121_GEX_T1 \
+--transcriptome=/well/htseq/Genomes/refdata-cellranger-2020-A/refdata-gex-GRCh38-2020-A
